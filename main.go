4d842f81340dfbd2a14c11e312d8f2d370bd756b
Florian Schuett <flo.schuett@chello.at>
2025-05-30 13:48:10 +0200

    Add REPL logic

--- main.go
+++ main.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		userInput := cleanInput(scanner.Text())
+		if len(userInput) == 0 {
+			continue
+		}
+		if userInput[0] == "quit" {
+			fmt.Println("Exiting Pokedex ...")
+			break
+		}
+
+		fmt.Printf("Your command was: %v\n", userInput[0])
+	}
+
 }
 
 func cleanInput(text string) []string {
